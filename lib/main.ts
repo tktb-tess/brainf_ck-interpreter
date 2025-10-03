10cdc485f956a1129fc4d048e0a3eb8fc961dd58
Born-Bohrer <chem1000leaves@gmail.com>
2025-10-03 15:33:19 +0900

    ittan

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,43 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener('click', () => setCounter(++counter));
-  setCounter(0);
+const commands = ['>', '<', '+', '-', '.', ',', '[', ']'] as const;
+
+export class BFInterpreter {
+  #memory: Uint8Array<ArrayBuffer>;
+  #ptr_: Uint32Array<ArrayBuffer>;
+
+  constructor(bufferLength: number = 30000) {
+    this.#memory = new Uint8Array(bufferLength);
+    this.#ptr_ = new Uint32Array(1);
+  }
+
+  get #ptr() {
+    return this.#ptr_.at(0)!;
+  }
+
+  set #ptr(v: number) {
+    if (v < 0 || v >= 2 ** 32) {
+      throw Error('ptr value is out of range');
+    }
+    this.#ptr_[0] = v;
+  }
+
+  #show() {
+    const v = this.#memory.at(this.#ptr);
+    if (v === undefined) {
+      throw SyntaxError('referenced memory is out of range');
+    }
+    return String.fromCharCode(v);
+  }
+
+  #increment() {
+    this.#ptr += 1;
+    if (this.#ptr >= this.#memory.length) {
+      const newMemory = new Uint8Array(this.#ptr * 2);
+
+      for (let i = 0; i < this.#memory.length; i++) {
+        newMemory[i] = this.#memory.at(i)!;
+      }
+
+      this.#memory = newMemory;
+    }
+  }
 }
