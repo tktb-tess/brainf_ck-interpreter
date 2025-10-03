dd897c480f2272942ef4f562405aeb8b70843d09
Born-Bohrer <chem1000leaves@gmail.com>
2025-10-04 02:10:50 +0900

    eee

--- lib/main.ts
+++ lib/main.ts
@@ -1,43 +1,145 @@
-const commands = ['>', '<', '+', '-', '.', ',', '[', ']'] as const;
+// 各命令の文字に対応するASCII(UTF-8)文字コード
+const POINTER_INCREMENT = 0x3e;
+const POINTER_DECREMENT = 0x3c;
+const VALUE_INCREMENT = 0x2b;
+const VALUE_DECREMENT = 0x2d;
+const WRITE_CHAR = 0x2e;
+const READ_CHAR = 0x2c;
+const LOOP_START = 0x5b;
+const LOOP_END = 0x5d;
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+const isCommand = (charCode: number) => {
+  return (
+    charCode === POINTER_INCREMENT ||
+    charCode === POINTER_DECREMENT ||
+    charCode === VALUE_INCREMENT ||
+    charCode === VALUE_DECREMENT ||
+    charCode === WRITE_CHAR ||
+    charCode === READ_CHAR ||
+    charCode === LOOP_START ||
+    charCode === LOOP_END
+  );
+};
 
 export class BFInterpreter {
   #memory: Uint8Array<ArrayBuffer>;
-  #ptr_: Uint32Array<ArrayBuffer>;
+  #ptr: number;
 
   constructor(bufferLength: number = 30000) {
     this.#memory = new Uint8Array(bufferLength);
-    this.#ptr_ = new Uint32Array(1);
-  }
-
-  get #ptr() {
-    return this.#ptr_.at(0)!;
-  }
-
-  set #ptr(v: number) {
-    if (v < 0 || v >= 2 ** 32) {
-      throw Error('ptr value is out of range');
-    }
-    this.#ptr_[0] = v;
+    this.#ptr = 0;
   }
 
   #show() {
     const v = this.#memory.at(this.#ptr);
     if (v === undefined) {
-      throw SyntaxError('referenced memory is out of range');
+      throw Error('referenced memory is out of range');
     }
-    return String.fromCharCode(v);
+    return v;
   }
 
   #increment() {
     this.#ptr += 1;
-    if (this.#ptr >= this.#memory.length) {
-      const newMemory = new Uint8Array(this.#ptr * 2);
 
-      for (let i = 0; i < this.#memory.length; i++) {
-        newMemory[i] = this.#memory.at(i)!;
+    if (this.#ptr >= 2 ** 32) {
+      throw Error('ptr address is out of range');
+    }
+
+    if (this.#ptr >= this.#memory.length) {
+      const newBuffSize = this.#memory.length * 2;
+
+      if (newBuffSize >= 2 ** 32) {
+        throw Error('memory size exceeds a maximum size');
+      }
+      const newMemory = new Uint8Array(newBuffSize);
+      const entries = this.#memory.entries();
+
+      for (const [i, v] of entries) {
+        newMemory[i] = v;
       }
 
       this.#memory = newMemory;
     }
+  }
+
+  #decrement() {
+    if (this.#ptr === 0) {
+      return;
+    }
+    this.#ptr -= 1;
+  }
+
+  #vIncrement() {
+    this.#memory[this.#ptr]++;
+  }
+
+  #vDecrement() {
+    this.#memory[this.#ptr]--;
+  }
+
+  execute(code: string, input: string) {
+    const output: number[] = [];
+    const inputBytes = encoder.encode(input);
+    let inputPtr = 0;
+    const codeBytes = encoder.encode(code);
+    let codePtr = 0;
+
+    loop: while (codePtr < codeBytes.length) {
+      const cmd = codeBytes[codePtr];
+
+      // コマンドではないときは全てコメント扱い
+      if (!isCommand(cmd)) {
+        ++codePtr;
+        continue loop;
+      }
+
+      switch (cmd) {
+        case POINTER_INCREMENT: {
+          this.#increment();
+          ++codePtr;
+          continue loop;
+        }
+        case POINTER_DECREMENT: {
+          this.#decrement();
+          ++codePtr;
+          continue loop;
+        }
+        case VALUE_INCREMENT: {
+          this.#vIncrement();
+          ++codePtr;
+          continue loop;
+        }
+        case VALUE_DECREMENT: {
+          this.#vDecrement();
+          ++codePtr;
+          continue loop;
+        }
+        case WRITE_CHAR: {
+          output.push(this.#show());
+          ++codePtr;
+          continue loop;
+        }
+        case READ_CHAR: {
+          this.#memory[this.#ptr] = inputBytes.at(inputPtr) ?? 0;
+          ++inputPtr;
+          ++codePtr;
+          continue loop;
+        }
+        case LOOP_START: {
+          break;
+        }
+        case LOOP_END: {
+          break;
+        }
+        default: {
+          throw Error('cannot reach here');
+        }
+      }
+    }
+
+    return decoder.decode(Uint8Array.from(output));
   }
 }
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+const app = document.getElementById('app') as HTMLDivElement;
+
+app.innerHTML = `<p>Hello!</p>`;
